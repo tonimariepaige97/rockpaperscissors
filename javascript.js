e9d0f8ffad93944fd264ee1b71f18d74b9e2b288
tonimariepaige97 <tonimariepaige97@gmail.com>
2024-01-20 14:44:40 -0500

    created a function that returns the choices randomly

--- javascript.js
+++ javascript.js
@@ -9,7 +9,16 @@
 // there will only be two players, the person and the computer
 
 // put the three choices into an array
+let choices = ["rock", "paper", "scissors"];
 
 // create a function called getComputerChoice that will allow the computer to have a random choice of rock, paper or scissors
+function getComputerChoice() {
+  // create a random number between 0 and 3
+  let randomNumber = Math.random() * 3;
+  // rounding the random number to a whole number
+  let randomNumberRounded = Math.floor(randomNumber);
 
-let choices = ["rock", "paper", "scissors"];
+  return choices[randomNumberRounded];
+}
+
+console.log(getComputerChoice());
